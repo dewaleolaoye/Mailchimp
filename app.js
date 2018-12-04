deb94f0af3cbf0d0034e35a16ccafd2dfbeb95ac
Adewale Olaoye <adewaleolaoye21@gmail.com>
2018-12-04 16:53:58 +0100

    setup

--- app.js
+++ app.js
@@ -20,6 +20,43 @@ app.post('/signup', (req, res) => {
         res.redirect('/fail.html');
         return;
     }
+
+     // Construct req Data
+     const data = {
+         members: [
+             {
+                 email_address: email,
+                 status: 'subscribed',
+                 merge_fields: {
+                     FNAME: firstname,
+                     LNAME: lastName
+                 }
+             }
+         ]
+     }
+
+     const postData = JSON.stringify(data);
+
+    const options = {
+        url: 'https://us19.api.mailchimp.com/3.0/lists/84f152a1c6',
+        method: 'POST',
+        headers: {
+            Authorization: 'auth c354a3933661d7fef7fb0c9bcd121f50-us19'
+        },
+        body: postData 
+    }
+
+    request(options, (err, response, body) => {
+        if(err){
+            res.redirect('/fail.html');
+        } else {
+            if (response.statusCode === 200) {
+                res.redirect('/success.html')
+            } else {
+                res.redirect('/fail.html');
+            }
+        }
+    });
 });
 
 
