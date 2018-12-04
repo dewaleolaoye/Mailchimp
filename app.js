ef32d0f58864229eb5e395a8bce9e40c67625234
Adewale Olaoye <adewaleolaoye21@gmail.com>
2018-12-04 16:27:44 +0100

    Signup route

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const path = require('path');
 
 const app = express();
 
+// Bodyparser Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Static Folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Signup Route
+app.post('/signup', (req, res) => {
+    const {firstname, lastName, email} = req.body;
+
+    // Make Sure fields are filled
+    if (!firstname || !lastName || !email) {
+        res.redirect('/fail.html');
+        return;
+    }
+});
 
 
 
